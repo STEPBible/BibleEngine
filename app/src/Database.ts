4f6732fc1751c2d899c789a72daf36100d45ed7f
Daniel Bennett <danbenn@umich.edu>
2019-05-30 22:07:23 -0400

    check that db exists before loading app

--- app/src/Database.ts
+++ app/src/Database.ts
@@ -4,13 +4,9 @@ import store from 'react-native-simple-store';
 export default class Database {
   public static async load(databaseModule: any) {
     const sqliteDirectory = `${Expo.FileSystem.documentDirectory}SQLite`;
-    const { exists, isDirectory } = await Expo.FileSystem.getInfoAsync(
-      sqliteDirectory
-    );
+    const { exists } = await Expo.FileSystem.getInfoAsync(sqliteDirectory);
     if (!exists) {
       await Expo.FileSystem.makeDirectoryAsync(sqliteDirectory);
-    } else if (!isDirectory) {
-      throw new Error('SQLite dir is not a directory');
     }
 
     const pathToDownloadTo = `${sqliteDirectory}/bibles.db`;
@@ -18,8 +14,12 @@ export default class Database {
     const { hash: incomingHash, uri: uriToDownload } = Expo.Asset.fromModule(
       databaseModule
     );
+
+    const { exists: fileExists } = await Expo.FileSystem.getInfoAsync(
+      pathToDownloadTo
+    );
     const existingHash = await store.get('existingHash');
-    if (incomingHash !== existingHash) {
+    if (!fileExists || incomingHash !== existingHash) {
       console.log('Updating database...');
       await Expo.FileSystem.downloadAsync(uriToDownload, pathToDownloadTo);
       store.save('existingHash', incomingHash);
