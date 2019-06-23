38141725bc77119489bf0d9154f0b6eb6437c4d9
Daniel Bennett <danbenn@umich.edu>
2019-06-22 22:40:48 -0400

    deduplicate chapter fetching code

--- app/src/Database.ts
+++ app/src/Database.ts
@@ -1,36 +1,38 @@
-import * as Expo from 'expo';
-import store from 'react-native-simple-store';
+import { BibleEngine } from '@bible-engine/core';
+import { ChapterResult } from './types';
 
 export default class Database {
-  public static async load(databaseModule: any) {
-    const sqliteDirectory = `${Expo.FileSystem.documentDirectory}SQLite`;
-    const { exists } = await Expo.FileSystem.getInfoAsync(sqliteDirectory);
-    if (!exists) {
-      await Expo.FileSystem.makeDirectoryAsync(sqliteDirectory);
+  public static async getChapter(
+    sqlBible: BibleEngine,
+    bookOsisId: string,
+    versionChapterNum: number
+  ) {
+    let chapterOutput;
+    try {
+      chapterOutput = await sqlBible.getFullDataForReferenceRange(
+        {
+          bookOsisId,
+          versionChapterNum,
+          versionUid: 'ESV'
+        },
+        true
+      );
+      let nextChapter;
+      try {
+        nextChapter = chapterOutput.contextRanges.normalizedChapter.nextRange;
+      } catch (e) {}
+      const result: ChapterResult = {
+        nextChapter,
+        contents: chapterOutput.content.contents
+      };
+      return result;
+    } catch (error) {
+      // TODO: fall back to network on database error
+      throw error;
     }
-
-    const pathToDownloadTo = `${sqliteDirectory}/bibles.db`;
-
-    const { hash: incomingHash, uri: uriToDownload } = Expo.Asset.fromModule(
-      databaseModule
-    );
-
-    const { exists: fileExists } = await Expo.FileSystem.getInfoAsync(
-      pathToDownloadTo
-    );
-    const existingHash = await store.get('existingHash');
-    if (!fileExists || incomingHash !== existingHash) {
-      console.log('Updating database...');
-      await Expo.FileSystem.deleteAsync(pathToDownloadTo, { idempotent: true });
-      await Expo.FileSystem.downloadAsync(uriToDownload, pathToDownloadTo);
-      store.save('existingHash', incomingHash);
-    }
-
-    const expoDatabase = await Expo.SQLite.openDatabase('bibles.db');
-    return expoDatabase;
   }
 
-  public static async executeSql(database, sql, params = []) {
+  public async executeSql(database: any, sql: any, params = []) {
     return new Promise((resolve, reject) =>
       database.transaction(tx => {
         tx.executeSql(
