7ba43a3b01293a8314fe8b7612b8397e42a6f945
Daniel Bennett <danbenn@umich.edu>
2019-07-21 18:23:32 -0400

    add script for building heroku database

---
+++ importers/scripts/buildHerokuDatabase.ts
@@ -0,0 +1,25 @@
+import { SwordImporter } from './../src/osis-sword-module/src/importer';
+import { StepLexiconImporter } from './../src/step-lexicon/importer';
+import { BeDatabaseCreator } from './../src/BeDatabaseCreator.class';
+
+const run = async () => {
+    console.log(process.env.DATABASE_URL)
+  const creator = new BeDatabaseCreator({
+    type: 'postgres',
+    url: process.env.DATABASE_URL,
+    ssl: true,
+    dropSchema: true
+  });
+
+  creator.addImporter(SwordImporter, {
+    sourcePath: 'src/osis-sword-module/data/ESV2016_th.zip'
+  });
+  creator.addImporter(SwordImporter, {
+    sourcePath: 'src/osis-sword-module/data/ChiUns.zip'
+  });
+  creator.addImporter(StepLexiconImporter)
+
+  await creator.createDatabase();
+};
+
+run();
--- importers/src/index.ts
+++ importers/src/index.ts
@@ -3,3 +3,4 @@ export * from './BeImportFileCreator.class';
 export * from './NEUE';
 export * from './osis-sword-module';
 export * from './v11n-rules';
+export * from './step-lexicon';
