e1034cbacc2626e53ad7ff5a913d4d7e262a0eaf
Daniel Bennett <danbenn@umich.edu>
2019-09-21 16:31:19 -0400

    fix compile errors for sqlite build script

--- importers/scripts/buildSqliteDatabase.ts
+++ importers/scripts/buildSqliteDatabase.ts
@@ -3,7 +3,6 @@ import { StepLexiconImporter } from './../src/step-lexicon/importer';
 import { BeDatabaseCreator } from './../src/BeDatabaseCreator.class';
 
 const run = async () => {
-    console.log(process.env.DATABASE_URL)
   const creator = new BeDatabaseCreator({
     type: 'sqlite',
     database: 'bibles.db',
@@ -12,16 +11,6 @@ const run = async () => {
 
   creator.addImporter(SwordImporter, {
     sourcePath: 'src/osis-sword-module/data/ESV2016_th.zip'
-  });
-  creator.addImporter(SwordImporter, {
-    sourcePath: 'src/osis-sword-module/data/ChiUns.zip',
-    versionMeta: {
-      title: 'Chinese Union Version',
-      uid: 'CUVs',
-      copyrightShort: 'Asia Bible Society 2013',
-      language: 'zh',
-      hasStrongs: true
-    }
   });
   creator.addImporter(StepLexiconImporter)
 
