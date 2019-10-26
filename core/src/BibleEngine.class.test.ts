ea75f986dbdf2f67b86c81a380ec3a237a89c5e1
Daniel Bennett <danbenn@umich.edu>
2019-10-25 23:40:34 -0400

    Refactor test database to clear between test runs

--- core/src/BibleEngine.class.test.ts
+++ core/src/BibleEngine.class.test.ts
@@ -1,29 +1,40 @@
 import { BibleEngine } from './BibleEngine.class';
 import { BibleVersionEntity } from './entities';
+import { getConnection } from 'typeorm';
 
-const sqlBible = new BibleEngine({
-    type: 'sqlite',
-    database: ':memory:'
-});
-let versionEntity: BibleVersionEntity | undefined;
+describe('BibleEngine', () => {
+    let sqlBible: BibleEngine;
+    beforeEach(() => {
+        sqlBible = new BibleEngine({
+            type: 'sqlite',
+            database: ':memory:'
+        });
+    });
+    afterEach(() => {
+        getConnection().close();
+    });
+    describe('BibleVersion', () => {
+        let versionEntity: BibleVersionEntity | undefined;
 
-beforeAll(async () => {
-    await sqlBible.addVersion(
-        new BibleVersionEntity({
-            uid: 'ESV',
-            title: 'English Standard Version',
-            language: 'en-US',
-            chapterVerseSeparator: ':'
-        })
-    );
-    versionEntity = await sqlBible.getVersion('ESV');
-});
+        beforeEach(async () => {
+            await sqlBible.addVersion(
+                new BibleVersionEntity({
+                    uid: 'ESV',
+                    title: 'English Standard Version',
+                    language: 'en-US',
+                    chapterVerseSeparator: ':'
+                })
+            );
+            versionEntity = await sqlBible.getVersion('ESV');
+        });
 
-test('BibleEngine version is set correctly', async () => {
-    expect(versionEntity).toBeDefined();
-    if (versionEntity) {
-        expect(versionEntity.language).toEqual('en-US');
-        expect(versionEntity.title).toEqual('English Standard Version');
-        expect(versionEntity.uid).toEqual('ESV');
-    }
+        test('BibleEngine version is set correctly', async () => {
+            expect(versionEntity).toBeDefined();
+            if (versionEntity) {
+                expect(versionEntity.language).toEqual('en-US');
+                expect(versionEntity.title).toEqual('English Standard Version');
+                expect(versionEntity.uid).toEqual('ESV');
+            }
+        });
+    });
 });
