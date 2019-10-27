2f1381ec0afa5641db86df8ef13c0b5a04e193d7
Daniel Bennett <danbenn@umich.edu>
2019-10-27 00:09:56 -0400

    Add download progress bar to versions screen

--- app/src/VersionScreen.tsx
+++ app/src/VersionScreen.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
-import { StyleSheet } from 'react-native'
-import { List, Button } from 'react-native-paper'
+import { StyleSheet, View } from 'react-native'
+import { List, Button, ProgressBar } from 'react-native-paper'
 import { ScrollView } from 'react-native-gesture-handler'
 import hoistNonReactStatics from 'hoist-non-react-statics'
 import { BibleVersionEntity } from '@bible-engine/core'
@@ -8,7 +8,7 @@ import { BibleVersionEntity } from '@bible-engine/core'
 import { withGlobalContext } from './GlobalContext'
 import { Color } from './Constants'
 
-class VersionScreen extends React.Component {
+class VersionScreen extends React.Component<any, any> {
   static navigationOptions = {
     headerTitle: 'Versions',
   }
@@ -17,12 +17,23 @@ class VersionScreen extends React.Component {
   }
 
   renderDownloadIcon = (version: BibleVersionEntity) => {
+    if (version.uid === this.props.global.versionUidOfDownload) {
+      return (
+        <View style={styles.progressBar}>
+          <ProgressBar
+            progress={this.props.global.downloadCompletionPercentage}
+            color={Color.TYNDALE_BLUE}
+          />
+        </View>
+      )
+    }
     if (version.dataLocation === 'remote') {
       return (
         <Button
+          disabled={!!this.props.global.versionUidOfDownload}
           style={styles.downloadButton}
           color={Color.TYNDALE_BLUE}
-          onPress={() => {}}
+          onPress={() => this.props.global.downloadVersion(version.uid)}
         >
           DOWNLOAD
         </Button>
@@ -53,6 +64,12 @@ class VersionScreen extends React.Component {
 const styles = StyleSheet.create({
   downloadButton: {
     marginTop: 8,
+  },
+  progressBar: {
+    maxWidth: 50,
+    flex: 1,
+    justifyContent: 'center',
+    marginRight: 8,
   },
 })
 
