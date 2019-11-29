18e02eb2560924d092277561190b315f861e62dd
Daniel Bennett <danbenn@umich.edu>
2019-11-28 21:25:47 -0500

    Remove downlod logic for a later PR

--- core/src/BibleEngine.class.test.ts
+++ core/src/BibleEngine.class.test.ts
@@ -1,6 +1,4 @@
-import { BookWithContentForInput } from './models/BibleInput';
 import { IBibleVersion } from './models/BibleVersion';
-import { IBibleBookEntity } from './models/BibleBook';
 import { BibleEngine } from './BibleEngine.class';
 import { getConnection } from 'typeorm';
 
@@ -10,41 +8,6 @@ const BIBLE_VERSION: IBibleVersion = {
     language: 'en-US',
     chapterVerseSeparator: ':',
     dataLocation: 'remote'
-};
-const BIBLE_BOOK: IBibleBookEntity = {
-    type: 'ot',
-    dataLocation: 'importing',
-    osisId: 'Gen',
-    abbreviation: 'Gen',
-    number: 0,
-    title: 'Genesis',
-    chaptersCount: [1],
-    versionId: 1
-};
-const BOOK_INPUT: BookWithContentForInput = {
-    book: {
-        osisId: 'Jude',
-        number: 65,
-        abbreviation: 'Jude',
-        title: 'Jude',
-        type: 'nt',
-        chaptersCount: [25]
-    },
-    contents: [
-        {
-            content: 'Jesus',
-            strongs: ['G2424'],
-            numbering: {
-                normalizedChapterIsStarting: 1,
-                normalizedChapterIsStartingInRange: 1,
-                normalizedVerseIsStarting: 1,
-                versionChapterIsStarting: 1,
-                versionChapterIsStartingInRange: 1,
-                versionVerseIsStarting: 1
-            }
-        }
-    ],
-    contentHasNormalizedNumbers: true
 };
 
 describe('BibleEngine', () => {
@@ -57,41 +20,6 @@ describe('BibleEngine', () => {
     });
     afterEach(async () => {
         await getConnection().close();
-    });
-    describe('versionIsDownloaded', () => {
-        test('If version doesnt exist in database at all, mark not downloaded', async () => {
-            expect(await sqlBible.versionIsDownloaded('ESV', '')).not.toBeTruthy();
-        });
-        test('If version is only remote, mark not downloaded', async () => {
-            await sqlBible.addVersion(BIBLE_VERSION);
-            expect(await sqlBible.versionIsDownloaded('ESV', '')).not.toBeTruthy();
-        });
-        test('If a version is only partially imported, mark not downloaded', async () => {
-            await sqlBible.addVersion({ ...BIBLE_VERSION, dataLocation: 'db' });
-            await sqlBible.addBook(BIBLE_BOOK);
-            expect(await sqlBible.versionIsDownloaded('ESV', '')).not.toBeTruthy();
-        });
-        test('If not all books are present in database, mark not downloaded', async () => {
-            jest.spyOn(BibleEngine.prototype, 'getBookIndexFile').mockResolvedValue([{}, {}]);
-            await sqlBible.addVersion({ ...BIBLE_VERSION, dataLocation: 'db' });
-            await sqlBible.addBook({ ...BIBLE_BOOK, dataLocation: 'db' });
-            expect(await sqlBible.versionIsDownloaded('ESV', '')).not.toBeTruthy();
-        });
-    });
-
-    describe('addBookWithContent', () => {
-        test('addBookWithContent safely skips phrases that already exist', async () => {
-            const version = await sqlBible.addVersion({ ...BIBLE_VERSION, dataLocation: 'db' });
-            const entityManager = await sqlBible.pDB;
-            const result = await sqlBible.addBookWithContent(version, BOOK_INPUT, {
-                entityManager
-            });
-            const result2 = await sqlBible.addBookWithContent(version, BOOK_INPUT, {
-                entityManager
-            });
-            expect(result).toBeTruthy();
-            expect(result2).toBeTruthy();
-        });
     });
 
     describe('BibleVersion', () => {
