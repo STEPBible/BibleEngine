c91f3dda7ca96f290b89624ed0944540f57b6b10
Daniel Bennett <danbenn@umich.edu>
2019-12-30 00:57:46 -0500

    Fix migration compatibility issues by dividing by db type

--- core/src/migrations/index.ts
+++
@@ -1,3 +0,0 @@
-import { InitialMigration1577681016410 } from './1577681016410-InitialMigration';
-
-export default [InitialMigration1577681016410];
---
+++ core/src/migrations/sqlite/index.ts
@@ -0,0 +1,3 @@
+import { InitialMigration1577684427757 } from './1577684427757-InitialMigration';
+
+export default [InitialMigration1577684427757];
