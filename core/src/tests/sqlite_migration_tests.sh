28ec6f4c3fbdaa2b617bc0fbb8a95ebea56461bd
Daniel Bennett <danbenn@umich.edu>
2020-02-09 15:12:53 -0500

    Add script to test for missing sqlite migrations

---
+++ core/src/tests/sqlite_migration_tests.sh
@@ -0,0 +1,12 @@
+set -e
+# Run existing migrations
+yarn typeorm migration:run --connection sqlite
+# Check if any additional migrations are needed
+yarn typeorm migration:generate -n AMissingMigration --connection sqlite
+# Test if the migration was created, and if so, return with error
+if ls ~/BibleEngine/core/src/migrations/sqlite/*AMissingMigration.ts 1> /dev/null 2>&1; then
+    echo "Schema changes were made that arent captured by migrations, exiting with error"
+    echo "Run the ./generate_migrations.sh file to create all needed migrations"
+    exit 1
+fi
+echo "Success! Current sqlite migrations account for all schema changes."
