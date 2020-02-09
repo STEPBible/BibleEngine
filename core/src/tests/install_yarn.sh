6b829344e374a2547b24fecb56d523039980d133
Daniel Bennett <danbenn@umich.edu>
2020-02-09 15:50:36 -0500

    Use modern version of node when installing Yarn

--- core/src/tests/install_yarn.sh
+++ core/src/tests/install_yarn.sh
@@ -3,3 +3,5 @@ set -e
 curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
 echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
 sudo apt update && sudo apt install --no-install-recommends yarn
+# Yarn requires a modern version of Node, >6
+source /opt/circleci/.nvm/nvm.sh && nvm install 13.5.0 && nvm use 13.5.0
