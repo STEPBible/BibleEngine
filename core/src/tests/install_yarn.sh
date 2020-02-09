af1fa7c87bda7902a2370b31dfb735d6c55f60d6
Daniel Bennett <danbenn@umich.edu>
2020-02-09 16:00:52 -0500

    Support using nvm on any line of job

--- core/src/tests/install_yarn.sh
+++ core/src/tests/install_yarn.sh
@@ -3,5 +3,5 @@ set -e
 curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
 echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
 sudo apt update && sudo apt install --no-install-recommends yarn
-# Yarn requires a modern version of Node, >6
+# Yarn requires a modern version of Node, >8
 source /opt/circleci/.nvm/nvm.sh && nvm install 13.5.0 && nvm use 13.5.0
