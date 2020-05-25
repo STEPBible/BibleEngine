1425b26dd150f7234722ad4eb2aa8fb749381ef2
Daniel Bennett <danbenn@umich.edu>
2020-05-25 15:44:57 -0500

    Implement book and chapter tab navigation

--- step-bible/src/views/References.vue
+++ step-bible/src/views/References.vue
@@ -1,5 +1,37 @@
 <template>
-    <div class="about">
-        <h1>This is an about page</h1>
-    </div>
+    <v-app>
+        <v-app-bar app collapse-on-scroll>
+            <v-icon>mdi-arrow-left</v-icon>
+            <v-spacer></v-spacer>
+            <v-toolbar-title>References</v-toolbar-title>
+            <v-spacer></v-spacer>
+        </v-app-bar>
+        <v-content>
+            <v-tabs v-model="tab" dark centered grow>
+                <v-tab>
+                    Books
+                </v-tab>
+                <v-tab>
+                    Chapters
+                </v-tab>
+            </v-tabs>
+            <v-tabs-items v-model="tab">
+                <v-tab-item>
+                    Books
+                </v-tab-item>
+                <v-tab-item>
+                    Chapters
+                </v-tab-item>
+            </v-tabs-items>
+        </v-content>
+    </v-app>
 </template>
+<script>
+export default {
+    data() {
+        return {
+            tab: null
+        };
+    }
+};
+</script>
