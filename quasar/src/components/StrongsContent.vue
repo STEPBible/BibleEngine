17f58fafaa71042a32b88eef43987a692165f2ea
Daniel Bennett <danbenn@umich.edu>
2020-08-30 20:24:56 -0400

    Switch to flex layout

--- quasar/src/components/StrongsContent.vue
+++ quasar/src/components/StrongsContent.vue
@@ -1,21 +1,21 @@
 <template>
-  <span>
-    <span v-if="isPhrase" class="phrase">{{ item.content }}</span>
-    <span v-else-if="isBoldedGroup">
+  <div>
+    <div v-if="isPhrase" class="phrase">{{ item.content }}</div>
+    <div v-else-if="isBoldedGroup">
       <span
         v-for="(phrase, phraseIndex) in childPhrases"
         :key="`bolded-group-${phraseIndex}`"
         class="phrase--bold"
       >{{ phrase }}</span>
-    </span>
-    <div v-else-if="isGroup">
+    </div>
+    <div v-else-if="isGroup" class="group">
       <strongs-content
         v-for="(content, index) in item.contents"
         :key="`content-${index}`"
         :item="content"
       />
     </div>
-  </span>
+  </div>
 </template>
 <script lang="ts">
 import Vue from 'vue';
@@ -46,5 +46,9 @@ export default Vue.extend({
 <style scoped>
 .phrase--bold {
   font-weight: bold;
+}
+.group {
+  display: flex;
+  overflow: scroll;
 }
 </style>
