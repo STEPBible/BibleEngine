f86203e8a5fbbd4ec5d393b0db61e726804fadf8
Daniel Bennett <danbenn@umich.edu>
2020-09-05 13:14:28 -0400

    Revert "Add vercel deployment support"
    This reverts commit 472d9f15b4d4780c50d484e2c4332fb3a2a4ca6a.

--- quasar/quasar.conf.js
+++ quasar/quasar.conf.js
@@ -51,7 +51,7 @@ module.exports = configure(function (ctx) {
     // Full list of options: https://quasar.dev/quasar-cli/quasar-conf-js#Property%3A-build
     build: {
       vueRouterMode: 'hash', // available values: 'hash', 'history'
-      distDir: 'public',
+
       // transpile: false,
 
       // Add dependencies for transpiling with Babel (Array of string/regex)
