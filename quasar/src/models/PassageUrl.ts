3278758d1ed355e914f4c6ea718913359d52aa8d
Daniel Bennett <danbenn@umich.edu>
2020-09-07 16:44:25 -0400

    Support using spaces as separator for passages

--- quasar/src/models/PassageUrl.ts
+++ quasar/src/models/PassageUrl.ts
@@ -5,9 +5,13 @@ export class PassageUrl {
 
     constructor(passage: string) {
         const bookAndChapter = passage.split(':')[0];
-        const pieces = bookAndChapter.split('+');
+        const pieces = bookAndChapter.split(/\+|\ /g);
         this.book = pieces.slice(0, pieces.length - 1).join('');
         this.chapter = pieces[pieces.length - 1];
         this.verse = passage.split(':')?.[1];
+    }
+
+    static build(book: string, chapter: string, verse: string) {
+        return `${book}+${chapter}+${verse}`
     }
 }
--- quasar/src/tests/PassageUrl.test.ts
+++ quasar/src/tests/PassageUrl.test.ts
@@ -1,14 +1,33 @@
 import { PassageUrl } from '../models/PassageUrl'
 
 describe('PassageUrl', () => {
+    const BOOK = 'Matt'
+    const CHAPTER = '15'
+    const VERSE = '10'
     it('accurately breaks up book and chapter and verse', () => {
+        const SEPARATOR = '+'
+        const PASSAGE = `${BOOK}${SEPARATOR}${CHAPTER}:${VERSE}`
+        const { book, chapter, verse } = new PassageUrl(PASSAGE)
+        expect(book).toBe(BOOK)
+        expect(chapter).toBe(CHAPTER)
+        expect(verse).toBe(VERSE)
+    })
+    it(`Supports spaces as a separator, too`, () => {
+        const SEPARATOR = ' '
+        const PASSAGE = `${BOOK}${SEPARATOR}${CHAPTER}:${VERSE}`
+        const { book, chapter, verse } = new PassageUrl(PASSAGE)
+        expect(book).toBe(BOOK)
+        expect(chapter).toBe(CHAPTER)
+        expect(verse).toBe(VERSE)
+    })
+})
+
+describe('build', () => {
+    it(`constructs a url out of book, chapter, verse`, () => {
         const BOOK = 'Matt'
         const CHAPTER = '5'
         const VERSE = '10'
-        const PASSAGE = `${BOOK}+${CHAPTER}:${VERSE}`
-        const url = new PassageUrl(PASSAGE)
-        expect(url.book).toBe(BOOK)
-        expect(url.chapter).toBe(CHAPTER)
-        expect(url.verse).toBe(VERSE)
+        const url = PassageUrl.build(BOOK, CHAPTER, VERSE)
+        expect(url).toBe(`${BOOK}+${CHAPTER}+${VERSE}`)
     })
 })
