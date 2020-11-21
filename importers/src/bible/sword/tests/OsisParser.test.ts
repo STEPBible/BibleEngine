2e4c90f3493f469a21f2e4aba37cb36ade40d016
Daniel Bennett <danbenn@umich.edu>
2020-11-21 16:09:21 -0500

    Add test cases for quotation mark handling

--- importers/src/bible/sword/tests/OsisParser.test.ts
+++ importers/src/bible/sword/tests/OsisParser.test.ts
@@ -1,3 +1,4 @@
+import { IBibleContent } from '@bible-engine/core';
 import { getBibleEngineInputFromXML } from '../src/OsisParser';
 import { ChapterXML } from '../src/types';
 
@@ -54,26 +55,140 @@ describe('OsisParser', () => {
         });
     });
 
-    it(`Correctly parses multiple strongs numbers`, () => {
-        const VERSE = `
-            <w lemma="strong:H0168 strong:H0413 strong:H9033 strong:H9006">
-                tent
-            </w>
-        `
-        const XML = createSingleVerseXmlObject(VERSE)
-        const json: any = getBibleEngineInputFromXML(XML)
-        const strongs = json[0].contents[0].strongs
-        expect(strongs).toStrictEqual(['H0168', 'H0413', 'H9033', 'H9006'])
+    describe('Strongs number parsing', () => {
+        it(`Correctly parses multiple strongs numbers`, () => {
+            const VERSE = `
+                <w lemma='strong:H0168 strong:H0413 strong:H9033 strong:H9006'>
+                    tent
+                </w>
+            `
+            const XML = createSingleVerseXmlObject(VERSE)
+            const json: any = getBibleEngineInputFromXML(XML)
+            const strongs = json[0].contents[0].strongs
+            expect(strongs).toStrictEqual(['H0168', 'H0413', 'H9033', 'H9006'])
+        })
+        it(`Correctly parses a single strongs number`, () => {
+            const VERSE = `
+                <w lemma='strong:H0168'>
+                    tent
+                </w>
+            `
+            const XML = createSingleVerseXmlObject(VERSE)
+            const json: any = getBibleEngineInputFromXML(XML)
+            const strongs = json[0].contents[0].strongs
+            expect(strongs).toStrictEqual(['H0168'])
+        })
     })
-    it(`Correctly parses a single strongs number`, () => {
-        const VERSE = `
-            <w lemma="strong:H0168">
-                tent
-            </w>
-        `
-        const XML = createSingleVerseXmlObject(VERSE)
-        const json: any = getBibleEngineInputFromXML(XML)
-        const strongs = json[0].contents[0].strongs
-        expect(strongs).toStrictEqual(['H0168'])
+
+    describe('Quotation mark parsing', () => {
+        it(`Wraps quote blocks in quotation marks`, () => {
+            const VERSE = `
+                <w lemma='strong:H0430'>
+                    God
+                </w>
+                <w lemma='strong:H0559'>
+                    said
+                </w>
+                ,
+                <q level='1' marker='“'/>
+                Let there
+                <w lemma='strong:H1961'>
+                    be
+                </w>
+                <w lemma='strong:H0216'>
+                    light
+                </w>
+                ,
+                <q eID='01001003.1' level='1' marker='”'/>
+                and there was
+                <w lemma='strong:H0216 strong:H1961'>
+                    light
+                </w>
+            `
+            const EXPECTED_JSON: IBibleContent[] = [
+                {
+                    type: 'section',
+                    contents: [
+                        {
+                            type: 'phrase',
+                            content: 'God',
+                            versionChapterNum: 1,
+                            versionVerseNum: 1,
+                            strongs: [
+                                'H0430'
+                            ]
+                        },
+                        {
+                            type: 'phrase',
+                            content: 'said,',
+                            versionChapterNum: 1,
+                            versionVerseNum: 1,
+                            strongs: [
+                                'H0559'
+                            ]
+                        },
+                        {
+                            type: 'phrase',
+                            content: '“',
+                            skipSpace: 'after',
+                            versionChapterNum: 1,
+                            versionVerseNum: 1
+                        },
+                        {
+                            type: 'phrase',
+                            content: 'Let there',
+                            versionChapterNum: 1,
+                            versionVerseNum: 1
+                        },
+                        {
+                            type: 'phrase',
+                            content: 'be',
+                            versionChapterNum: 1,
+                            versionVerseNum: 1,
+                            strongs: [
+                                'H1961'
+                            ]
+                        },
+                        {
+                            type: 'phrase',
+                            content: 'light,',
+                            versionChapterNum: 1,
+                            versionVerseNum: 1,
+                            strongs: [
+                                'H0216'
+                            ]
+                        },
+                        {
+                            type: 'phrase',
+                            content: '”',
+                            skipSpace: 'before',
+                            versionChapterNum: 1,
+                            versionVerseNum: 1
+                        },
+                        {
+                            type: 'phrase',
+                            content: 'and there was',
+                            versionChapterNum: 1,
+                            versionVerseNum: 1
+                        },
+                        {
+                            type: 'phrase',
+                            content: 'light',
+                            versionChapterNum: 1,
+                            versionVerseNum: 1,
+                            strongs: [
+                                'H0216',
+                                'H1961'
+                            ]
+                        }
+                    ]
+                }
+            ]
+            const XML = createSingleVerseXmlObject(VERSE)
+            const json: any = getBibleEngineInputFromXML(XML)
+            expect(json).toStrictEqual(EXPECTED_JSON)
+        })
     })
+
+
 })
