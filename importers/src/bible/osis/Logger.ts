8a09c5cc772843c4b6584fea5d2ce6cd6d79a8b1
Daniel Bennett <danbenn@umich.edu>
2020-11-29 18:15:59 -0600

    Add module for logging different log levels

---
+++ importers/src/bible/osis/Logger.ts
@@ -0,0 +1,32 @@
+import * as winston from 'winston'
+type LogLevel = 'verbose' | 'info' | 'warning' | 'error'
+
+class Logger {
+    logger: winston.Logger
+
+    constructor() {
+        const level: LogLevel = (process.env.LOG as LogLevel) || 'warning'
+        this.logger = winston.createLogger({
+            level,
+            transports: new winston.transports.Console()
+        });
+    }
+
+    error(message: string, ...meta: any[]) {
+        this.logger.error(message, meta)
+    }
+
+    warning(message: string, ...meta: any[]) {
+        this.logger.warning(message, meta)
+    }
+
+    info(message: string, ...meta: any[]) {
+        this.logger.info(message, meta)
+    }
+
+    verbose(message: string, ...meta: any[]) {
+        this.logger.verbose(message, meta)
+    }
+}
+
+export default new Logger()
