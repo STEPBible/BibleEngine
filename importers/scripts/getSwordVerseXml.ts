16b2108569e87426ee078764e87464727c887f93
Daniel Bennett <danbenn@umich.edu>
2020-12-12 22:18:42 -0600

    Improve argument passing to script

--- importers/scripts/getSwordVerseXml.ts
+++ importers/scripts/getSwordVerseXml.ts
@@ -4,10 +4,19 @@ const prettifyXML = require('xml-formatter');
 import ModuleIndex from '../src/bible/sword/src/ModuleIndex';
 import SwordModule from '../src/bible/sword/src/SwordModule';
 
-const IMPORT_PATH = 'src/bible/sword/data/ESV2016_th.zip'
-const REFERENCE = `${process.env.BOOK} ${process.env.CHAPTER}`
+const IMPORT_PATH = process.env.SWORD_PATH // Ex: 'src/bible/sword/data/SomeModule.zip'
+const REFERENCE = `${process.env.BOOK} ${process.env.CHAPTER}` // Ex: 'Gen 1'
 
 const main = async () => {
+    if (!IMPORT_PATH) {
+        throw new Error('Missing IMPORT_PATH argument')
+    }
+    if (!process.env.BOOK) {
+        throw new Error('Missing BOOK argument')
+    }
+    if (!process.env.CHAPTER) {
+        throw new Error('Missing CHAPTER number argument')
+    }
     const contents = readFileSync(IMPORT_PATH);
     const fileIndex = ModuleIndex.fromNodeBuffer(contents);
     const swordModule = new SwordModule(fileIndex);
@@ -25,4 +34,4 @@ const main = async () => {
     console.log(prettifyXML(textWithSingleRootNode))
 }
 
-main().catch(error => console.log('Got an error: ', error.message))
+main().catch(error => console.log(error))
