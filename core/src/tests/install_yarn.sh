3435c37b2977f5bc5d8c7533d9fcbad4fca0f5c4
Daniel Bennett <danbenn@umich.edu>
2020-12-19 14:54:28 -0600

    Upgrade node to fix mysql error

--- core/src/tests/install_yarn.sh
+++ core/src/tests/install_yarn.sh
@@ -3,4 +3,4 @@ set -e
 curl -o- -L https://yarnpkg.com/install.sh | bash
 echo 'export PATH="$HOME/.yarn/bin:$HOME/.config/yarn/global/node_modules/.bin:$PATH"' >> $BASH_ENV
 # Yarn requires a modern version of Node, >8
-source /opt/circleci/.nvm/nvm.sh && nvm install 13.5.0 && nvm use 13.5.0
+source /opt/circleci/.nvm/nvm.sh && nvm install 14.15.3 && nvm use 14.15.3
