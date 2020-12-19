96ee5b3627349dbd5fb2288c0eab8f9d1ae8fc4a
Daniel Bennett <danbenn@umich.edu>
2020-12-19 14:35:59 -0600

    Fix missing yarn dependency in integration tests

--- core/src/tests/install_yarn.sh
+++ core/src/tests/install_yarn.sh
@@ -5,3 +5,5 @@ echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/source
 sudo apt update && sudo apt install --no-install-recommends yarn
 # Yarn requires a modern version of Node, >8
 source /opt/circleci/.nvm/nvm.sh && nvm install 13.5.0 && nvm use 13.5.0
+# Make Yarn available to bash
+echo 'export PATH="$HOME/.yarn/bin:$HOME/.config/yarn/global/node_modules/.bin:$PATH"' >> $BASH_ENV
