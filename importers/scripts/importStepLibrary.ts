83ac4363883a99a706e47a57a9e5c6fe9537b928
Daniel Bennett <danbenn@umich.edu>
2020-12-20 17:32:43 -0600

    Add functionality to download sword urls

--- importers/scripts/importStepLibrary.ts
+++ importers/scripts/importStepLibrary.ts
@@ -1,15 +1,28 @@
 import { S3 } from '@aws-sdk/client-s3'
+import { createWriteStream } from 'fs'
+import { get } from 'http'
 
 const PUBLIC_BUCKET_NAME = 'tyndale-house-public'
 const REGION = 'eu-west-1'
 
 const main = async () => {
-    await getSwordModuleDownloadUrls(PUBLIC_BUCKET_NAME)
+    const urls = await getSwordModuleDownloadUrls(PUBLIC_BUCKET_NAME)
+    urls.forEach(url => downloadSwordFile(url))
+}
+
+const downloadSwordFile = (url: string) => {
+    const pieces = url.split('/')
+    const filename = pieces[pieces.length - 1]
+    const path = `data/step-library/${filename}`
+    const file = createWriteStream(path);
+    return get(url, (response) => {
+        response.pipe(file);
+    })
 }
 
 const getSwordModuleDownloadUrls = async (bucketName: string) => {
     const s3 = new S3({ region: REGION });
-    const S3_BASE_URL = `https://${bucketName}.s3-${REGION}.amazonaws.com`
+    const S3_BASE_URL = `http://${bucketName}.s3-${REGION}.amazonaws.com`
     const { Contents } = await s3.listObjects({ Bucket: PUBLIC_BUCKET_NAME })
     if (!Contents) {
         throw new Error('Bucket has no contents')
