ec5a38bc262a3c564f526c76a7a2d206aa7af966
Daniel Bennett <danbenn@umich.edu>
2020-12-27 15:09:52 -0600

    Stop adding esv to pre-built database to allow user choice

--- importers/scripts/buildSqliteDatabase.ts
+++ importers/scripts/buildSqliteDatabase.ts
@@ -1,5 +1,4 @@
 import { V11nImporter } from './../src/stepdata/v11n-rules/index';
-import { SwordImporter } from './../src/bible/sword/src/importer';
 import { StepLexiconImporter } from './../src/stepdata/step-lexicon/importer';
 import { BeDatabaseCreator } from './../src/BeDatabaseCreator.class';
 
@@ -9,9 +8,6 @@ const run = async () => {
         database: 'bibles.db'
     });
     creator.addImporter(V11nImporter);
-    creator.addImporter(SwordImporter, {
-        sourcePath: 'src/bible/sword/data/ESV2016_th.zip'
-    });
     creator.addImporter(StepLexiconImporter);
 
     await creator.createDatabase();
