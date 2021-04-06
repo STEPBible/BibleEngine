ba6ec59978e3578b5447b12fcc5139c51169f34a
Daniel Bennett <danbenn@umich.edu>
2021-04-05 22:48:32 -0400

    Update formatting and metadata of sword imports

--- importers/scripts/importStepLibrary.ts
+++ importers/scripts/importStepLibrary.ts
@@ -1,34 +1,51 @@
-import { createWriteStream, mkdirSync } from 'fs'
-import { get } from 'http'
-import { S3 } from '@aws-sdk/client-s3'
+import { createWriteStream, mkdirSync } from 'fs';
+import { get } from 'http';
+import { S3 } from '@aws-sdk/client-s3';
 import { BeDatabaseCreator, OsisImporter, StepLexiconImporter } from '../src';
 import { SwordImporter } from './../src/bible/sword/src/importer';
 import { ConnectionOptions } from 'typeorm';
-import { zhBookMetadata, zhsBookMetadata, esBookMetdata, enBookMetadata } from './../src/metadata'
+import { zhBookMetadata, zhsBookMetadata, esBookMetdata, enBookMetadata } from './../src/metadata';
 
-const BUCKETS = [
-    'tyndale-house-public',
-    'tyndale-house-private'
-]
-const REGION = 'eu-west-1'
-const LOCAL_CACHE_PATH = 'data/step-library'
+const BUCKETS = ['tyndale-house-public', 'tyndale-house-private'];
+const REGION = 'eu-west-1';
+const LOCAL_CACHE_PATH = 'data/step-library';
 
 const CONNECTION_OPTIONS: ConnectionOptions = {
     type: 'better-sqlite3',
-    database: 'bibles.db'
-}
+    database: 'bibles.db',
+};
 
 const main = async () => {
     if (process.env.SKIP_CACHE) {
-        await downloadAllStepModules()
+        await downloadAllStepModules();
     }
     const creator = new BeDatabaseCreator(CONNECTION_OPTIONS);
+    creator.addImporter(SwordImporter, {
+        sourcePath: `${LOCAL_CACHE_PATH}/KJV.zip`,
+        versionMeta: {
+            uid: 'KJV',
+            title: 'King James Version (1769)',
+            isPlaintext: false,
+            hasStrongs: true,
+        },
+    });
+
+    creator.addImporter(SwordImporter, {
+        sourcePath: `${LOCAL_CACHE_PATH}/spaRV1909eb.zip`,
+        bookMeta: esBookMetdata,
+        versionMeta: {
+            uid: 'RV1909',
+            title: 'Reina-Valera 1909',
+            isPlaintext: false,
+            hasStrongs: true,
+        },
+    });
     creator.addImporter(OsisImporter, {
-        sourcePath: `${LOCAL_CACHE_PATH}/NASB_TH_osis.xml`,
+        sourcePath: `${LOCAL_CACHE_PATH}/sblg_th.xml`,
         bookMeta: enBookMetadata,
         versionMeta: {
-            uid: 'NASB',
-            title: 'New American Standard Bible',
+            uid: 'SBLGNT',
+            title: 'The Greek New Testament: SBLGNT upgraded by Tyndale House',
             isPlaintext: false,
             hasStrongs: true,
         },
@@ -41,25 +58,27 @@ const main = async () => {
             title: 'English Standard Version',
             isPlaintext: false,
             hasStrongs: true,
-        }
+        },
     });
+    creator.addImporter(OsisImporter, {
+        sourcePath: `${LOCAL_CACHE_PATH}/NASB_TH_osis.xml`,
+        bookMeta: enBookMetadata,
+        versionMeta: {
+            uid: 'NASB',
+            title: 'New American Standard Bible',
+            isPlaintext: false,
+            hasStrongs: true,
+        },
+    });
+
     creator.addImporter(SwordImporter, {
         sourcePath: `${LOCAL_CACHE_PATH}/OSHB.zip`,
         versionMeta: {
             uid: 'OHB',
             title: 'Open Scriptures Hebrew Bible',
-            isPlaintext: true,
+            isPlaintext: false,
             hasStrongs: true,
-        }
-    });
-    creator.addImporter(SwordImporter, {
-        sourcePath: `${LOCAL_CACHE_PATH}/sblg-the.zip`,
-        versionMeta: {
-            uid: 'SBLGNT',
-            title: 'The Greek New Testament: SBLGNT upgraded by Tyndale House',
-            isPlaintext: true,
-            hasStrongs: true,
-        }
+        },
     });
     creator.addImporter(SwordImporter, {
         sourcePath: `${LOCAL_CACHE_PATH}/ChiUn.zip`,
@@ -67,9 +86,9 @@ const main = async () => {
         versionMeta: {
             uid: '和合本 (繁)',
             title: '和合本 (繁體字)',
-            isPlaintext: true,
+            isPlaintext: false,
             hasStrongs: true,
-        }
+        },
     });
     creator.addImporter(SwordImporter, {
         sourcePath: `${LOCAL_CACHE_PATH}/ChiUns.zip`,
@@ -77,75 +96,44 @@ const main = async () => {
         versionMeta: {
             uid: '和合本 (简)',
             title: '和合本 (简体字)',
-            isPlaintext: true,
+            isPlaintext: false,
             hasStrongs: true,
-        }
-    });
-    creator.addImporter(SwordImporter, {
-        sourcePath: `${LOCAL_CACHE_PATH}/hlt2018eb.zip`,
-        versionMeta: {
-            uid: 'MCSB',
-            title: 'Baibal Olcim',
-            isPlaintext: true,
-            hasStrongs: true,
-        }
-    });
-    creator.addImporter(SwordImporter, {
-        sourcePath: `${LOCAL_CACHE_PATH}/KJV.zip`,
-        versionMeta: {
-            uid: 'KJV',
-            title: 'King James Version (1769)',
-            isPlaintext: true,
-            hasStrongs: true,
-        }
+        },
     });
 
-    creator.addImporter(SwordImporter, {
-        sourcePath: `${LOCAL_CACHE_PATH}/spaRV1909eb.zip`,
-        bookMeta: esBookMetdata,
-        versionMeta: {
-            uid: 'RV1909',
-            title: 'Reina-Valera 1909',
-            isPlaintext: true,
-            hasStrongs: true,
-        }
-    });
     creator.addImporter(StepLexiconImporter);
-    await creator.createDatabase()
-}
+    await creator.createDatabase();
+};
 
 const downloadAllStepModules = async () => {
-    mkdirSync(LOCAL_CACHE_PATH, { recursive: true })
+    mkdirSync(LOCAL_CACHE_PATH, { recursive: true });
     for (const bucket of BUCKETS) {
-        const urls = await getSwordModuleDownloadUrls(bucket)
-        console.log(urls)
-        urls.forEach(url => downloadSwordFile(url))
+        const urls = await getSwordModuleDownloadUrls(bucket);
+        console.log(urls);
+        urls.forEach((url) => downloadSwordFile(url));
     }
-}
+};
 
 const downloadSwordFile = (url: string) => {
-    const pieces = url.split('/')
-    const filename = pieces[pieces.length - 1]
-    const path = `${LOCAL_CACHE_PATH}/${filename}`
+    const pieces = url.split('/');
+    const filename = pieces[pieces.length - 1];
+    const path = `${LOCAL_CACHE_PATH}/${filename}`;
     const file = createWriteStream(path);
     return get(url, (response) => {
         response.pipe(file);
-    })
-}
+    });
+};
 
 const getSwordModuleDownloadUrls = async (bucketName: string) => {
     const s3 = new S3({ region: REGION });
-    const S3_BASE_URL = `http://${bucketName}.s3-${REGION}.amazonaws.com`
-    const { Contents } = await s3.listObjects({ Bucket: bucketName })
+    const S3_BASE_URL = `http://${bucketName}.s3-${REGION}.amazonaws.com`;
+    const { Contents } = await s3.listObjects({ Bucket: bucketName });
     if (!Contents) {
-        throw new Error('Bucket has no contents')
+        throw new Error('Bucket has no contents');
     }
-    return Contents
-        .map(object => object.Key)
-        .filter(key => (
-            key?.includes('.zip')
-        ))
-        .map(key => `${S3_BASE_URL}/${key}`)
-}
+    return Contents.map((object) => object.Key)
+        .filter((key) => key?.includes('.zip'))
+        .map((key) => `${S3_BASE_URL}/${key}`);
+};
 
-main().catch(error => console.log(error))
+main().catch((error) => console.log(error));
