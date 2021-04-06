e5202d56d50a7d822a55e1f3fff913bbe2be6515
Daniel Bennett <danbenn@umich.edu>
2021-04-05 22:51:17 -0400

    Remove unused code

--- importers/scripts/importStepLibrary.ts
+++ importers/scripts/importStepLibrary.ts
@@ -1,13 +1,8 @@
-import { createWriteStream, mkdirSync } from 'fs';
-import { get } from 'http';
-import { S3 } from '@aws-sdk/client-s3';
 import { BeDatabaseCreator, OsisImporter, StepLexiconImporter } from '../src';
 import { SwordImporter } from './../src/bible/sword/src/importer';
 import { ConnectionOptions } from 'typeorm';
 import { zhBookMetadata, zhsBookMetadata, esBookMetdata, enBookMetadata } from './../src/metadata';
 
-const BUCKETS = ['tyndale-house-public', 'tyndale-house-private'];
-const REGION = 'eu-west-1';
 const LOCAL_CACHE_PATH = 'data/step-library';
 
 const CONNECTION_OPTIONS: ConnectionOptions = {
@@ -103,37 +98,6 @@ const main = async () => {
 
     creator.addImporter(StepLexiconImporter);
     await creator.createDatabase();
-};
-
-const downloadAllStepModules = async () => {
-    mkdirSync(LOCAL_CACHE_PATH, { recursive: true });
-    for (const bucket of BUCKETS) {
-        const urls = await getSwordModuleDownloadUrls(bucket);
-        console.log(urls);
-        urls.forEach((url) => downloadSwordFile(url));
-    }
-};
-
-const downloadSwordFile = (url: string) => {
-    const pieces = url.split('/');
-    const filename = pieces[pieces.length - 1];
-    const path = `${LOCAL_CACHE_PATH}/${filename}`;
-    const file = createWriteStream(path);
-    return get(url, (response) => {
-        response.pipe(file);
-    });
-};
-
-const getSwordModuleDownloadUrls = async (bucketName: string) => {
-    const s3 = new S3({ region: REGION });
-    const S3_BASE_URL = `http://${bucketName}.s3-${REGION}.amazonaws.com`;
-    const { Contents } = await s3.listObjects({ Bucket: bucketName });
-    if (!Contents) {
-        throw new Error('Bucket has no contents');
-    }
-    return Contents.map((object) => object.Key)
-        .filter((key) => key?.includes('.zip'))
-        .map((key) => `${S3_BASE_URL}/${key}`);
 };
 
 main().catch((error) => console.log(error));
