d24cba987d97418375078bee01b547bce7721332
Daniel Bennett <danbenn@umich.edu>
2021-12-29 16:10:44 -0500

    Update script to create app sqlite database

--- importers/scripts/buildSqliteDatabases.ts
+++ importers/scripts/buildSqliteDatabases.ts
@@ -1,16 +1,20 @@
-import { ConnectionOptions } from 'typeorm';
+import { SwordImporter } from './../src/bible/sword/src/importer';
+import { zhsBookMetadata } from './../src/metadata/index';
 import { BeDatabaseCreator, OsisImporter, V11nImporter } from '../src';
 import { enBookMetadata } from '../src/metadata';
 
 const LOCAL_CACHE_PATH = 'data/step-library';
 
-const CONNECTION_OPTIONS: ConnectionOptions = {
-    type: 'better-sqlite3',
-    database: 'bibles.db',
+const main = async () => {
+    await Promise.all([createEsvDatabase(), createCuvDatabase()])
 };
 
-const main = async () => {
-    const creator = new BeDatabaseCreator(CONNECTION_OPTIONS);
+const createEsvDatabase = async () => {
+    const creator = new BeDatabaseCreator({
+        name: 'esv-connection',
+        type: 'better-sqlite3',
+        database: 'esv.db',
+    });
     creator.addImporter(OsisImporter, {
         sourcePath: `${LOCAL_CACHE_PATH}/ESV2016_OSIS+Strongs.xml`,
         bookMeta: enBookMetadata,
@@ -23,6 +27,26 @@ const main = async () => {
     });
     creator.addImporter(V11nImporter);
     await creator.createDatabase();
-};
+}
+
+const createCuvDatabase = async () => {
+    const creator = new BeDatabaseCreator({
+        name: 'cuv-connection',
+        type: 'better-sqlite3',
+        database: 'cuvs.db',
+    });
+    creator.addImporter(SwordImporter, {
+        sourcePath: `${LOCAL_CACHE_PATH}/ChiUns.zip`,
+        bookMeta: zhsBookMetadata,
+        versionMeta: {
+            uid: '和合本 (简)',
+            title: '和合本 (简体字)',
+            isPlaintext: false,
+            hasStrongs: true,
+        },
+    });
+    creator.addImporter(V11nImporter);
+    await creator.createDatabase();
+}
 
 main().catch((error) => console.log(error));
