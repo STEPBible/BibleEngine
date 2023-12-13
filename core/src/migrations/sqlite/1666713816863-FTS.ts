13da66280038d5f00df45e1458a5916ff7b17fb4
Daniel Bennett <danbenn@umich.edu>
2023-12-12 21:11:51 -0600

    Fix Expo migration issue caused by FTS5 extension

--- core/src/migrations/sqlite/1666713816863-FTS.ts
+++ core/src/migrations/sqlite/1666713816863-FTS.ts
@@ -4,9 +4,13 @@ export class FTS1666713816863 implements MigrationInterface {
     name = 'FTS1666713816863';
 
     public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(
-            `CREATE VIRTUAL TABLE bible_search USING fts5(verse, versionUid UNINDEXED, versionBook UNINDEXED, versionChapter UNINDEXED, versionVerse UNINDEXED);`
-        );
+        try {
+            await queryRunner.query(
+                `CREATE VIRTUAL TABLE bible_search USING fts5(verse, versionUid UNINDEXED, versionBook UNINDEXED, versionChapter UNINDEXED, versionVerse UNINDEXED);`
+            );
+        } catch (error) {
+            console.error('failed to create virtual table bible_search', error)
+        }
     }
 
     public async down(): Promise<void> {
