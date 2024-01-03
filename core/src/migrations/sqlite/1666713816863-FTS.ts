fd433d8e336ad3939ad1251e52af74d7faf59b9e
Daniel Bennett <danbenn@umich.edu>
2024-01-02 20:29:39 -0600

    update log message with more context

--- core/src/migrations/sqlite/1666713816863-FTS.ts
+++ core/src/migrations/sqlite/1666713816863-FTS.ts
@@ -9,7 +9,7 @@ export class FTS1666713816863 implements MigrationInterface {
                 `CREATE VIRTUAL TABLE bible_search USING fts5(verse, versionUid UNINDEXED, versionBook UNINDEXED, versionChapter UNINDEXED, versionVerse UNINDEXED);`
             );
         } catch (error) {
-            console.error('failed to create virtual table bible_search', error)
+            console.error(`Failed to create virtual table bible_search. This is an expected error if you use SQLite versions without fts support. It can be safely ignored as long as you don't enable/use full text search features in BibleEngine`, error)
         }
     }
 
