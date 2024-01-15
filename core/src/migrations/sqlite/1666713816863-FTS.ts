5fe23e7a705c26d7dd1ad9d911c3fb736c48a453
Daniel Bennett <12377258+danbenn@users.noreply.github.com>
2024-01-15 14:45:23 -0600

    Fix Expo migration issue caused by FTS5 extension (#211)
    * stop running broken migration tests from CI
    
    * Fix Expo migration issue caused by FTS5 extension
    
    * update log message with more context

--- core/src/migrations/sqlite/1666713816863-FTS.ts
+++ core/src/migrations/sqlite/1666713816863-FTS.ts
@@ -4,9 +4,13 @@ export class FTS1666713816863 implements MigrationInterface {
     name = 'FTS1666713816863';
 
     public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(
-            `CREATE VIRTUAL TABLE bible_search USING fts5(verse, versionUid UNINDEXED, versionBook UNINDEXED, versionChapter UNINDEXED, versionVerse UNINDEXED);`
-        );
+        try {
+            await queryRunner.query(
+                `CREATE VIRTUAL TABLE bible_search USING fts5(verse, versionUid UNINDEXED, versionBook UNINDEXED, versionChapter UNINDEXED, versionVerse UNINDEXED);`
+            );
+        } catch (error) {
+            console.error(`Failed to create virtual table bible_search. This is an expected error if you use SQLite versions without fts support. It can be safely ignored as long as you don't enable/use full text search features in BibleEngine`, error)
+        }
     }
 
     public async down(): Promise<void> {
